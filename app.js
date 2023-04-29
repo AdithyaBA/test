dcb1b750cbcb7c6ea0cb9ee7f9e8313ff660b453
adithyaba93@gmail.com <adithya.bendehakkalu@toyota.com>
2023-04-29 17:59:32 +0530

    added port

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const dotenv = require("");
 const mongoose = require("mongoose");
+
+// Create the DB connection
+
+// Set the PORT
+const PORT = process.env.PORT || 8000
 
 // Create the server
 app.listen(PORT, () => {
