c43341388725c10028b09cf224bbc177a2207cd6
adithyaba93@gmail.com <adithya.bendehakkalu@toyota.com>
2023-04-29 21:38:51 +0530

    encrypt password

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,15 @@
+const User = require("../model/user");
+const {validationResult} = require("express-validator");
+
+// Signup middleware
 exports.signup = (req, res, next) => {
-    res.send(req.body)
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        })
+    }
+    const user = new User(req.body);
+    console.log(user);
     next();
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {signup} = require("../controller/auth");
+const {check} = require("express-validator");
 
-router.post("/signup", signup);
+router.post("/signup", [
+    check("firstName", "Please enter the firstName").isLength({min: 3}),
+    check("lastName", "Please enter the lastName").isLength({min: 1}),
+    check("email", "Please enter the valid email").isEmail(),
+    check("password", "Enter the valid password").isLength({min: 4})
+], signup);
 
 module.exports = router;
